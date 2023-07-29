65014759fc938c83adbe3604fa8aa1c9cbf30bec
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-07-29 22:36:08 +0530

    addimg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date();
+  console.log(date);
+  date.setDate(date.getDate() + count);
+  // console.log(date);
+  return (
+    <div className="container">
+      <div className="stepA">
+        {/* <button onClick={() => setStep((c) => c - 1)}> Negative </button> */}
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
+        <span> Step : {step} </span>
+        {/* <button onClick={() => setStep((c) => c + 1)}>Positive</button> */}
+      </div>
+      <div className="stepB">
+        <button onClick={() => setCount((c) => c - step)}> Negative </button>
+        {/* <span> Count : {count} </span> */}
+        <input
+          className="inputMeddile"
+          type="number"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        />
+        <button onClick={() => setCount((c) => c + step)}>Positive</button>
+      </div>
+      <p className="paragraph">
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
